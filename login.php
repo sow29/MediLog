373d14695b766fa216db991ec4ab670454bbcf6c
sow29 <90741519+sow29@users.noreply.github.com>
2021-09-19 08:59:46 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 include "db_con.php";
@@ -7,21 +6,15 @@ include "db_con.php";
 if ( isset( $_POST[ 'Uname' ] ) && isset( $_POST[ 'Pass' ] ) ) {
 
   function validate( $data ) {
-
     $data = trim( $data );
-
     $data = stripslashes( $data );
-
     $data = htmlspecialchars( $data );
-
     return $data;
-
   }
 
   $uname = validate( $_POST[ 'Uname' ] );
-
   $pass = validate( $_POST[ 'Pass' ] );
-
+  
   $sql = "SELECT * FROM doctor where name = '$uname' AND password = md5('$pass')";
 
   $result = mysqli_query( $conn, $sql );
@@ -32,9 +25,7 @@ if ( isset( $_POST[ 'Uname' ] ) && isset( $_POST[ 'Pass' ] ) ) {
 
   } else {
 
-    //header( "Location: index.php?error=Incorect User name or password" );
     echo "password error";
-
     exit();
 
   }
